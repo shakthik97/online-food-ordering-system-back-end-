b753380a74eecea20b684d38d2996ccc8631e440
Chathuranga Shakthi <chathurangashakthi.k@gmail.com>
2024-08-09 02:19:21 +0530

    Resolved merge conflicts

---
+++ src/main/java/com/example/Online/Food/Ordering/model/User.java
@@ -0,0 +1,28 @@
+package com.example.Online.Food.Ordering.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role;
+}
