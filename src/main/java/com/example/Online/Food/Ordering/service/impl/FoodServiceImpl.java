b96fcab16febe258779476cd2530a290e73a3315
Chathuranga Shakthi <chathurangashakthi.k@gmail.com>
2024-08-19 18:32:54 +0530

    food filter methods completed

--- src/main/java/com/example/Online/Food/Ordering/service/impl/FoodServiceImpl.java
+++ src/main/java/com/example/Online/Food/Ordering/service/impl/FoodServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FoodServiceImpl implements FoodService {
@@ -38,12 +39,61 @@ public class FoodServiceImpl implements FoodService {
 
     @Override
     public void deleteFood(Long foodId) throws Exception {
+        Food food = findFoodById(foodId);
+        food.setRestaurant(null);
+        foodRepository.save(food);
 
     }
 
     @Override
-    public List<Food> getRestaurantFood(Long restaurantId, boolean isVegetarian, boolean isNonVeg, boolean isSeasonal, String foodCategory) {
-        return null;
+    public List<Food> getRestaurantFood(Long restaurantId,
+                                        boolean isVegetarian,
+                                        boolean isNonVeg,
+                                        boolean isSeasonal,
+                                        String foodCategory) {
+
+        List<Food> foods = foodRepository.findByRestaurantId(restaurantId);
+
+        if(isVegetarian){
+            foods = filterByVegetarian(foods, isVegetarian);
+        }
+        if(isNonVeg){
+            foods = filterByNonVeg(foods, isNonVeg);
+        }
+        if(isSeasonal){
+            foods = filterBySeasonal(foods, isSeasonal);
+        }
+        if(foodCategory != null && !foodCategory.equals("")){
+            foods = filterByFoodCategory(foods, foodCategory);
+        }
+        return foods;
+    }
+
+    private List<Food> filterByFoodCategory(List<Food> foods, String foodCategory) {
+        return foods.stream().filter(food -> {
+            if(food.getFoodCategory() != null){
+                return food.getFoodCategory().getName().equals(foodCategory);
+            }
+            return false;
+        }).collect(Collectors.toList());
+    }
+
+    private List<Food> filterBySeasonal(List<Food> foods, boolean isSeasonal) {
+        return foods.stream()
+                .filter(food -> food.isSeasonal()==isSeasonal)
+                .collect(Collectors.toList());
+    }
+
+    private List<Food> filterByNonVeg(List<Food> foods, boolean isNonVeg) {
+        return foods.stream()
+                .filter(food -> food.isVegetarian()==false)
+                .collect(Collectors.toList());
+    }
+
+    private List<Food> filterByVegetarian(List<Food> foods, boolean isVegetarian) {
+        return foods.stream()
+                .filter(food -> food.isVegetarian()==isVegetarian)
+                .collect(Collectors.toList());
     }
 
     @Override
