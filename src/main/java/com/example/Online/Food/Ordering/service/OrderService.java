4711710470da1fb9d9802c9ad1e7da62761cfce7
Chathuranga Shakthi <chathurangashakthi.k@gmail.com>
2024-08-21 10:29:24 +0530

    OrderServiceImpl - all methods completed

--- src/main/java/com/example/Online/Food/Ordering/service/OrderService.java
+++ src/main/java/com/example/Online/Food/Ordering/service/OrderService.java
@@ -17,4 +17,7 @@ public interface OrderService {
     public List<Order> getUserOrder(Long userId) throws Exception;
 
     public List<Order> getRestaurantsOrder(Long restaurantId, String orderStatus) throws Exception;
+
+    public Order findOrderById(Long orderId) throws Exception;
+
 }
